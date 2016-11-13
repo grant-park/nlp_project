885bf281da83c79b294d051c5e29071bcfabf227
sungjp <gpark18@amherst.edu>
2016-11-12 22:43:52 -0500

    first commit

---
+++ nb_tweet.py
@@ -0,0 +1,13 @@
+from __future__ import division
+
+import math
+import os
+
+from collections import defaultdict
+
+PATH_TO_DATA = ''
+TRAIN_DIR = os.path.join(PATH_TO_DATA, 'train')
+TEST_DIR = os.path.join(PATH_TO_DATA, 'test')
+def tokenize_doc(doc):
+
+
